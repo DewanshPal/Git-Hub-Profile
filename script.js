2991a3e9f618ef196ff61ca3734d80a5effccd02
Dewansh Pal <dewanshpal108@gmail.com>
2023-07-30 13:02:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const apiURL =  "https://api.github.com/users/";
+//async/await functions let's you write promise based code as per your requirements
+const main = document.querySelector("#main");
+const searchBox = document.getElementById("search");
+const getUser = async(username) => 
+{
+    const response = await fetch(apiURL + username);
+    const data = await response.json();
+    const card = `
+    <div class="card">
+            <div>
+                <img class="avatar" src="${data.avatar_url}" alt="Florin Pop">
+            </div>
+            <div class="user-info">
+                <h2>${data.name}</h2>
+                <p>${data.bio}</p>
+
+                <ul class="info">
+                    <li>${data.followers}<strong>Followers</strong></li>
+                    <li>${data.following}<strong>Following</strong></li>
+                    <li>${data.public_repos}<strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos">
+                </div>
+                
+                <a href="${data.html_url}" target="_blank"><button>Git-Hub-Profile</button></a>
+            </div>
+        </div>
+    `
+    main.innerHTML = card;
+    getRepos(username);
+}
+// getUser("DewanshPal");
+
+const getRepos = async(username) =>
+{
+    const repos = document.getElementById("repos");
+    const response = await fetch(apiURL + username + "/repos");
+    const data = await response.json();
+    for(let i=0;i<data.length;i++)
+    {
+        repos.innerHTML += `<a class="repo" href="${data[i].html_url}" target="_blank">${data[i].name}</a>`;       
+    }
+}
+
+const formSubmit = () =>
+{
+    if(searchBox.value != "")
+    {
+        getUser(searchBox.value);
+        searchBox.value = "";
+    }
+    return false;
+}
+
+searchBox.addEventListener("focusout" , () =>
+{
+    formSubmit();
+})
+searchBox.addEventListener("keypress" , (event) =>
+{
+    if(event.key === "Enter")
+    {
+        formSubmit();
+    }
+})
